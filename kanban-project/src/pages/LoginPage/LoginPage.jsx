52319e8fa196a2403edfc54a5642ba89c6d4f6d4
bbiomax <bbbiomax@gmail.com>
2024-02-18 19:29:35 +0300

    Login стили

--- kanban-project/src/pages/LoginPage/LoginPage.jsx
+++ kanban-project/src/pages/LoginPage/LoginPage.jsx
@@ -1,24 +1,28 @@
-export default function LoginPage() {
+import * as S from "./LoginPage.styled"
+
+export default function LoginPage({login}) {
     return (
-        <div className="wrapper">
-            <div className="container-signin">
-                <div className="modal">
-                    <div className="modal__block">
-                        <div className="modal__ttl">
-                            <h2>Вход</h2>
-                        </div>
-                        <form className="modal__form-login" id="formLogIn" action="#">
-                            <input className="modal__input" type="text" name="login" id="formlogin" placeholder="Эл. почта">
-                            <input className="modal__input" type="password" name="password" id="formpassword" placeholder="Пароль">
-                            <button className="modal__btn-enter _hover01" id="btnEnter"><a href="../main.html">Войти</a></button>
-                            <div className="modal__form-group">
-                                <p>Нужно зарегистрироваться?</p>
-                                <a href="signup.html">Регистрируйтесь здесь</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <S.BodySignIn>
+            <S.StyledWrapper>
+                <S.ContainerSignIn>
+                    <S.Modal>
+                        <S.ModalBlock>
+                            <S.ModalTtl>
+                                <h2>Вход</h2>
+                            </S.ModalTtl>
+                            <S.ModalFormLogin id="formLogIn" action="#">
+                                <S.ModalInput type="text" name="login" id="formlogin" placeholder="Эл. почта" />
+                                <S.ModalInput type="password" name="password" id="formpassword" placeholder="Пароль" />
+                                <S.ModalBtnEnter onClick={login} id="btnEnter">Войти</S.ModalBtnEnter>
+                                <S.ModalFormGroup>
+                                    <p>Нужно зарегистрироваться?</p>
+                                    <a href="signup.html">Регистрируйтесь здесь</a>
+                                </S.ModalFormGroup>
+                            </S.ModalFormLogin >
+                        </S.ModalBlock>
+                    </S.Modal>
+                </S.ContainerSignIn>
+            </S.StyledWrapper>
+        </S.BodySignIn>
     )
 }
